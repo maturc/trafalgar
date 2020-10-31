aa7a2c5771403640872f3af90ccbe1bd3f2ab408
Matija Turčec <turcec.matija@gmail.com>
2020-10-31 23:52:40 +0100

    added services component

---
+++ src/components/Services/Card.tsx
@@ -0,0 +1,29 @@
+import { Card as CardUI, CardContent, CardMedia, Typography } from '@material-ui/core';
+import React from 'react';
+import './Card.css';
+
+interface ICard {
+  img: string;
+  title: string;
+  body: string;
+}
+
+export default function Card( {img, title, body}:ICard ) {
+  return (
+    <CardUI className="card">
+      <CardMedia
+          className="card__media"
+          image={img}
+          title=""
+        />
+      <CardContent>
+        <Typography variant="h3">
+          {title}
+        </Typography>
+        <Typography variant="body1">
+          {body}
+        </Typography>
+      </CardContent>
+    </CardUI>
+  );
+}
---
+++ src/components/Services/Services.tsx
@@ -0,0 +1,62 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+import './Services.css';
+import { ButtonCustom } from '../shared/ButtonCustom';
+import Card from './Card';
+
+import searchDoctor from '../../assets/search-doctor.svg';
+import onlinePharmacy from '../../assets/online-pharmacy.svg';
+import consultation from '../../assets/consultation.svg';
+import detailsInfo from '../../assets/details-info.svg';
+import emergencyCare from '../../assets/emergency-care.svg';
+import tracking from '../../assets/tracking.svg';
+
+export function Services() {
+  return (
+    <section className="services">
+      <Typography variant="h2">
+        Our services
+      </Typography>
+      <hr/>
+      <Typography variant="body1">
+        We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors you can consult with us which type of service is suitable for your health
+      </Typography>
+      <div className="services__cards">
+        <Card
+          img={searchDoctor}
+          title="Search doctor"
+          body="Choose your doctor from thousands of specialist, general, and trusted hospitals"
+        />
+        <Card
+          img={onlinePharmacy}
+          title="Online pharmacy"
+          body="Buy  your medicines with our mobile application with a simple delivery system"
+        />
+        <Card
+          img={consultation}
+          title="Consultation"
+          body="Free consultation with our trusted doctors and get the best recomendations"
+        />
+        <Card
+          img={detailsInfo}
+          title="Details info"
+          body="Free consultation with our trusted doctors and get the best recomendations"
+        />
+        <Card
+          img={emergencyCare}
+          title="Emergency care"
+          body="You can get 24/7 urgent care for yourself or your children and your
+          lovely family"
+        />
+        <Card
+          img={tracking}
+          title="Tracking"
+          body="Track and save your medical history and health data "
+        />
+      </div>
+      <ButtonCustom buttonStyle="secondary">
+          Learn more
+      </ButtonCustom>
+    </section>
+  );
+}
