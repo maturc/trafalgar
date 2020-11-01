3dc8068ecf9bde9a2d8b746f4d361bc24265a469
Matija Turčec <turcec.matija@gmail.com>
2020-11-01 20:43:43 +0100

    added logo to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../../assets/logo.svg';
 
 export function Navbar() {
   return (
     <nav className="navbar">
-      <a href="/" className="navbar__logo">Logo</a>
+      <a href="/" className="navbar__logo" style={{backgroundImage: `url(${logo})`}}><span>Trafalgar</span></a>
       <ul className="navbar__links">
         <li><a href="/">Home         </a></li>
         <li><a href="/">Find a doctor</a></li>
