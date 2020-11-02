86fc41e55de3cc3d0bc20a68f71d981a75d371b4
Matija Turčec <turcec.matija@gmail.com>
2020-11-02 23:45:38 +0100

    updated styling

--- src/components/Testimonials/CarouselItem.tsx
+++ src/components/Testimonials/CarouselItem.tsx
@@ -12,17 +12,19 @@ interface ICarouselItem {
 export function CarouselItem( {img, name, company, quote}: ICarouselItem ) {
   return (
     <blockquote className="item">
-      <img src={img} alt="" />
-      <footer className="item__author">
-        <cite>
-          <Typography variant="h4" component="span" className="item__name">
-            {name}
-          </Typography>
-          <Typography variant="subtitle2" component="span" className="item__company">
-            {company}
-          </Typography>
-        </cite>
-      </footer>
+      <div className="item__person">
+        <img src={img} alt="" className="item__img"/>
+        <footer className="item__author">
+          <cite>
+            <Typography variant="h4" component="span" className="item__name">
+              {name}
+            </Typography>
+            <Typography variant="subtitle2" component="span" className="item__company">
+              {company}
+            </Typography>
+          </cite>
+        </footer>
+      </div>
       <Typography variant="body1">
         {quote}
       </Typography>
