0d6aed47a5b58c40217d7fc98d70ffc5979b8a29
Matija Turčec <turcec.matija@gmail.com>
2020-11-02 23:48:25 +0100

    updated styling and stuff

--- src/components/Articles/Article.tsx
+++ src/components/Articles/Article.tsx
@@ -1,6 +1,7 @@
-import { Typography } from '@material-ui/core';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
 import React from 'react';
 import { ButtonCustom } from '../shared/ButtonCustom';
+import './Article.css';
 
 interface IArticle {
   img:   string;
@@ -11,15 +12,22 @@ interface IArticle {
 
 export default function Article( {img, alt, title, body}: IArticle ) {
   return (
-    <div className="article">
-      <img src={img} alt={alt} />
-      <Typography variant="h3">
-        {title}
-      </Typography>
-      <Typography variant="body1">
-        {body}
-      </Typography>
-      <ButtonCustom>Read more →</ButtonCustom>
-    </div>
+    <Card className="article">
+      <CardMedia className="article__img"
+        component="img"
+        alt={alt}
+        height="240"
+        image={img}
+      />
+      <CardContent>
+        <Typography variant="h3">
+          {title}
+        </Typography>
+        <Typography variant="body2">
+          {body}
+        </Typography>
+        <ButtonCustom>Read more →</ButtonCustom>
+      </CardContent>
+    </Card>
   );
 }
--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -14,24 +14,26 @@ export function Articles() {
         Check out our latest article
       </Typography>
       <hr />
-      <Article
-        img={articleImage1}
-        alt="Doctor taking a blood sample from a patient"
-        title="Disease detection, check up in the laboratory"
-        body="In this case, the role of the health laboratory is very important to do a disease detection..."
-      />
-      <Article
-        img={articleImage2}
-        alt="Doctor using a microscope"
-        title="Herbal medicines that are safe for consumption"
-        body="Herbal medicine is very widely used at this time because of its very good for your health..."
-      />
-      <Article
-        img={articleImage3}
-        alt="Patient with a face mask"
-        title="Natural care for healthy facial skin"
-        body="A healthy lifestyle should start from now and also for your skin health. There are some..."
-      />
+      <div className="articles__container">
+        <Article
+          img={articleImage1}
+          alt="Doctor taking a blood sample from a patient"
+          title="Disease detection, check up in the laboratory"
+          body="In this case, the role of the health laboratory is very important to do a disease detection..."
+        />
+        <Article
+          img={articleImage2}
+          alt="Doctor using a microscope"
+          title="Herbal medicines that are safe for consumption"
+          body="Herbal medicine is very widely used at this time because of its very good for your health..."
+        />
+        <Article
+          img={articleImage3}
+          alt="Patient with a face mask"
+          title="Natural care for healthy facial skin"
+          body="A healthy lifestyle should start from now and also for your skin health. There are some..."
+        />
+      </div>
       <ButtonCustom>View all</ButtonCustom>
     </div>
   );
