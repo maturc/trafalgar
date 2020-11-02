e01c60adf7bc8951643cf56b73bd8a7f7611113c
Matija Turčec <turcec.matija@gmail.com>
2020-11-02 19:09:40 +0100

    add auto slide to carousel, styled & moved control

--- src/components/Testimonials/Carousel.tsx
+++ src/components/Testimonials/Carousel.tsx
@@ -1,24 +1,12 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import './Carousel.css';
 
 interface ICarousel {
+  index: number;
   children: Array<ReactElement>;
 }
 
-export function Carousel( {children}: ICarousel ) {
-  const [index, setIndex] = useState<number>(0);
-  function decrementIndex() {
-    let value = index-1;
-    if(value < 0)
-      value = children.length-1;
-    setIndex(value);
-  }
-  function incrementIndex() {
-    let value = index+1;
-    if(value >= children.length)
-      value = 0;
-    setIndex(value);
-  }
+export function Carousel( {index, children}: ICarousel ) {
   return (
     <div className="testimonials__carousel">
       <div 
@@ -27,8 +15,6 @@ export function Carousel( {children}: ICarousel ) {
       >
         {children}
       </div>
-      <button onClick={decrementIndex}>{"<"}</button>
-      <button onClick={incrementIndex}>{">"}</button>
     </div>
   );
 }
--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,49 +1,93 @@
 import { Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Carousel } from './Carousel';
 import { CarouselItem } from './CarouselItem';
 import './Testimonials.css';
+import controlsLeftIcon from '../../assets/carousel-left.svg';
+import controlsDotIcon from '../../assets/carousel-dot.svg';
 
 export function Testimonials() {
+  const [index, setIndex]       = useState<number>(0);
+  const [numberOfCarouselItems] = useState<number>(4);
+  let timer: NodeJS.Timeout;
+
+  function decrementIndex() {
+    let value = index-1;
+    if(value < 0)
+      value = numberOfCarouselItems-1;
+    setIndex(value);
+  }
+  function incrementIndex() {
+    let value = index+1;
+    if(value >= numberOfCarouselItems)
+      value = 0;
+    setIndex(value);
+  }
+
+  function startSlideTimer() {
+    timer = setTimeout(function(){ incrementIndex() }, 3000);
+  }
+  useEffect(() => {
+    startSlideTimer();
+    return () => clearTimeout(timer);
+  }, [index, startSlideTimer]); //eslint-disable-line
+
+  const controlsArrow: React.CSSProperties = { background: `url(${controlsLeftIcon})` }
+  const controlsDot:   React.CSSProperties = { background: `url(${controlsDotIcon})`  }
+  const dotIsActive1 = index===0 ? "testimonials__controls-dot--active" : "";
+  const dotIsActive2 = index===1 ? "testimonials__controls-dot--active" : "";
+  const dotIsActive3 = index===2 ? "testimonials__controls-dot--active" : "";
+  const dotIsActive4 = index===3 ? "testimonials__controls-dot--active" : "";
   return (
-    <div className="testimonials">
-      <Typography variant="h2">
-        What our customer are saying
-      </Typography>
-      <hr />
-      <Carousel>
-        <CarouselItem 
-          img=""
-          name="Edward Newgate"
-          company="Founder Circle"
-          quote="“Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely”"
-        />
-        <CarouselItem 
-          img=""
-          name="Aadasddward Nesdadswgate"
-          company="sdada Cidasdasdrcle"
-          quote="“Our ed pasdasdagement app and 
-          web portal allow asdasdyousdas toasda access information instantaneously (no tedeous form, long calls, 
-          or administrasecurely”"
-        />
-        <CarouselItem 
-          img=""
-          name="Bdwswgate"
-          company="Fossder Airsade"
-          quote="“Ogement snd 
-          web portal allow ormation instantaneously (no tedeous dlls, 
-          or administracurely”"
-        />
-        <CarouselItem 
-          img=""
-          name="Cdwte"
-          company="Foule"
-          quote="“Oed agement app and 
-          web portal allormation instantaneously (no tedeous form, long calls, 
-          or administrative hasly”"
-        />
-      </Carousel>
-     
-    </div>
+    <>
+      <div className="testimonials">
+        <Typography variant="h2">
+          What our customer are saying
+        </Typography>
+        <hr />
+        <Carousel
+          index={index}
+        >
+          <CarouselItem
+            img=""
+            name="Edward Newgate"
+            company="Founder Circle"
+            quote="“Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely”"
+          />
+          <CarouselItem
+            img=""
+            name="Aadasddward Nesdadswgate"
+            company="sdada Cidasdasdrcle"
+            quote="“Our ed pasdasdagement app and 
+            web portal allow asdasdyousdas toasda access information instantaneously (no tedeous form, long calls, 
+            or administrasecurely”"
+          />
+          <CarouselItem
+            img=""
+            name="Bdwswgate"
+            company="Fossder Airsade"
+            quote="“Ogement snd 
+            web portal allow ormation instantaneously (no tedeous dlls, 
+            or administracurely”"
+          />
+          <CarouselItem
+            img=""
+            name="Cdwte"
+            company="Foule"
+            quote="“Oed agement app and 
+            web portal allormation instantaneously (no tedeous form, long calls, 
+            or administrative hasly”"
+          />
+        </Carousel>
+      </div>
+      <div className="testimonials__controls">
+        <button onClick={decrementIndex}  className="testimonials__controls-left"  style={controlsArrow}></button>
+        <button onClick={()=>setIndex(0)} className={`testimonials__controls-dot ${dotIsActive1}`}   style={controlsDot}></button>
+        <button onClick={()=>setIndex(1)} className={`testimonials__controls-dot ${dotIsActive2}`}   style={controlsDot}></button>
+        <button onClick={()=>setIndex(2)} className={`testimonials__controls-dot ${dotIsActive3}`}   style={controlsDot}></button>
+        <button onClick={()=>setIndex(3)} className={`testimonials__controls-dot ${dotIsActive4}`}   style={controlsDot}></button>
+        <button onClick={incrementIndex}  className="testimonials__controls-right" style={controlsArrow}></button>
+      </div>
+    </>
   );
 }
