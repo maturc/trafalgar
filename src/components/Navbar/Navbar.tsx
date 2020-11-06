4588b0d9c0446fcfe5adc0cfc4a806e42bc34f39
Matija Turčec <turcec.matija@gmail.com>
2020-11-06 11:44:40 +0100

    responsive design for navbar and burger menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.svg';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton } from '@material-ui/core';
 
 export function Navbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
   return (
-    <nav className="navbar">
-      <a href="/" className="navbar__logo" style={{backgroundImage: `url(${logo})`}}><span>Trafalgar</span></a>
-      <ul className="navbar__links">
-        <li><a href="/" className="navbar__link--active">Home         </a></li>
-        <li><a href="/">Find a doctor</a></li>
-        <li><a href="/">Apps         </a></li>
-        <li><a href="/">Testimonials </a></li>
-        <li><a href="/">About us     </a></li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar">
+        <a href="/" className="navbar__logo" style={{backgroundImage: `url(${logo})`}}><span>Trafalgar</span></a>
+        <ul className={`navbar__links ${isOpen ? "navbar__burger--open" : ""}`}>
+          <li><a href="/" className="navbar__link--active">Home         </a></li>
+          <li><a href="/">Find a doctor</a></li>
+          <li><a href="/">Apps         </a></li>
+          <li><a href="/">Testimonials </a></li>
+          <li><a href="/">About us     </a></li>
+        </ul>
+        <nav className="navbar__burger">
+          <IconButton color="primary" aria-label="Open navigation menu" component="button" onClick={handleClick}>
+            <MenuIcon />
+          </IconButton>
+        </nav>
+      </nav>
+      
+    </>
   );
 }
